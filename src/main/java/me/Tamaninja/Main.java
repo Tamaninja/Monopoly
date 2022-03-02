61e8cbe2eb78c32805b13c6d937088005ca4dff1
Tamaninja <liel.king54@gmail.com>
2022-03-02 15:52:37 +0200

    Initial commit

---
+++ src/main/java/me/Tamaninja/Main.java
@@ -0,0 +1,7 @@
+package me.Tamaninja;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+    }
+}
---
+++ src/main/java/me/Tamaninja/MainFrame.java
@@ -0,0 +1,12 @@
+package me.Tamaninja;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    private int YResolution;
+    private int XResolution;
+    public MainFrame() {
+        this.YResolution = 
+        this.setVisible(true);
+    }
+}
